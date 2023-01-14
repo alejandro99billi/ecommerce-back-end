3e675413c02231100f520f8929798a69cea864ec
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-01-14 17:23:58 +0100

    prova

--- src/main/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplication.java
+++ src/main/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceMaaApplication {
-
+//inizio del progetto
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceMaaApplication.class, args);
+		System.out.println("hola");
 	}
 
 }
