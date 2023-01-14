68ebd8e2b10729d31adf681e53e3dececeac64c5
MikeDace <michaelspig@gmail.com>
2023-01-14 16:36:39 +0100

    First Commit

--- src/main/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplication.java
+++ src/main/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceMaaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Michael");
 		SpringApplication.run(EcommerceMaaApplication.class, args);
 	}
 
