210dd651fa6216c09cf3fd6859c9872b4feeaad1
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-01-14 17:05:48 +0100

    initial commit

---
+++ src/main/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplication.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.ecommerceMAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ecommerce/ecommerceMAA/EcommerceMaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.ecommerceMAA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
